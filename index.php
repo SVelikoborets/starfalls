27ae6875349a4e1178d330c86780c8afb1c95239
S.Velikoborets <safiya.soboleva.97@mail.ru>
2024-03-20 11:40:09 +0300

    modified index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ namespace Core;
 error_reporting(E_ALL);
 ini_set('display_errors','on');
 
-require_once $_SERVER['DOCUMENT_ROOT'].'/project/config/connection.php';
 spl_autoload_register();
 
 $routes = require $_SERVER['DOCUMENT_ROOT'].'/project/config/routes.php';
@@ -13,6 +12,7 @@ $track = ( new Router) ->getTrack($routes, $_SERVER['REQUEST_URI']);
 $page = (new Dispatcher) ->getPage($track);
 
 echo (new View)  ->render($page);
+
 
 
 
